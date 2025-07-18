0f8985abd9708150c5b771950075a72cc19afb33
salmaabdelnabi <35731964+salmaabdelnabi@users.noreply.github.com>
2025-07-19 00:58:04 +0300

    Created the Bot class.

---
+++ src/main/java/engine/Bot.java
@@ -0,0 +1,20 @@
+package engine;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+public class Bot {
+    WebDriver driver;
+    Wait<WebDriver> wait;
+
+    public Bot(){
+        driver = new ChromeDriver();
+        wait = new FluentWait<>(driver);
+    }
+
+    public void quit(){
+        driver.quit();
+    }
+}
