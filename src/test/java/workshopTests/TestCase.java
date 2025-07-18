8adc88e6e6cca798bc9d2ab6f42e4f3f5fa514cb
salmaabdelnabi <35731964+salmaabdelnabi@users.noreply.github.com>
2025-07-19 01:04:40 +0300

    Created the TestCase class.

---
+++ src/test/java/workshopTests/TestCase.java
@@ -0,0 +1,22 @@
+package workshopTests;
+
+import engine.Bot;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+@Test
+public class TestCase {
+    Bot bot;
+
+    @BeforeMethod
+    public void methodSetup(){
+        bot = new Bot();
+    }
+
+    @AfterMethod
+    public void methodTearDown(){
+        bot.quit();
+        bot = null;
+    }
+}
